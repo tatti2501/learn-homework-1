14eec527909781b54371be555c047dfd19141132
Tatyana <twinki2525@mail.ru>
2019-05-22 23:08:44 +0300

    добавила isinstance

--- if2.py
+++ if2.py
@@ -30,14 +30,22 @@ text2=input('И еще что-нибудь ')
 
 
 def main(text1, text2):
+    
     if text1==text2:
-        return(0)
-    if text1!=text2:
-        if len('text1')>len('text2'):
+        return(1)
+    elif text1!=text2:
+        if len(text1)>len(text2):
             return(2)
-    if text1!=text2:
+    elif text1!=text2:
         if text2=='learn':
             return(3)
+    
+    text1=isinstance(text1, str)
+    text2=isinstance(text2, str)
+    if text1 == True:
+        if text2 == True:
+            return (0)
+        
 
 name=main(text1, text2)
 print(name)
